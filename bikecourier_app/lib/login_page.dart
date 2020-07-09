a91a4d2b5f36c45ec267c0f339d93e50cb8e581c
Raimundo Vásquez Urdanivia <rvasquez15@alumnos.utalca.cl>
2020-07-09 19:53:22 -0400

    feat: first version of the loign page

---
+++ bikecourier_app/lib/login_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  Widget createLabel(double padding, String label) {
+    return Padding(
+      padding: EdgeInsets.only(top: padding),
+      child: Text(label,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          textAlign: TextAlign.center),
+    );
+  }
+
+  Widget createFormField(String hintText) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: hintText),
+    );
+  }
+
+  Widget createLoginButton() {
+    return Container(
+      padding: const EdgeInsets.only(top: 15),
+      child: RaisedButton(
+        color: Colors.black,
+        textColor: Colors.white,
+        child: Text('INGRESAR'),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        decoration: BoxDecoration(color: Color.fromRGBO(255, 251, 193, 1.0)),
+        child: ListView(children: [
+          Image.asset(
+            'assets/images/logo.png',
+            width: 96,
+            height: 96,
+          ),
+          createLabel(41, 'USUARIO'),
+          createFormField('usuario@ejemplo.com'),
+          createLabel(15, 'CONTRASEÑA'),
+          createFormField('Contraseña'),
+          createLoginButton(),
+        ]),
+      ),
+    );
+  }
+}

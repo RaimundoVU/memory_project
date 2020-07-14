b4e8eb6e7b6e6012a0f6daa863c8cef94402552a
Raimundo Vásquez Urdanivia <rvasquez15@alumnos.utalca.cl>
2020-07-13 23:49:39 -0400

    feat: Added firebase connection and login function

--- bikecourier_app/lib/login_page.dart
+++ bikecourier_app/lib/login_page.dart
@@ -1,6 +1,18 @@
+import 'package:bikecourier_app/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  
+  
   Widget createLabel(double padding, String label) {
     return Padding(
       padding: EdgeInsets.only(top: padding),
@@ -10,8 +22,27 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget createFormField(String hintText, bool isPassword) {
+  Widget createEmailFormField(String hintText, bool isPassword) {
     return TextFormField(
+      validator: (input) {
+        if (input.isEmpty) {
+          return 'Please type some email'; 
+        }
+      },
+      onSaved: (input) => _email = input,
+      obscureText: isPassword,
+      decoration: InputDecoration(hintText: hintText),
+    );
+  }
+
+  Widget createPasswordFormField(String hintText, bool isPassword) {
+      return TextFormField(
+      validator: (input) {
+        if (input.isEmpty) {
+          return 'Please type some password'; 
+        }
+      },
+      onSaved: (input) => _password = input,
       obscureText: isPassword,
       decoration: InputDecoration(hintText: hintText),
     );
@@ -24,7 +55,7 @@ class LoginPage extends StatelessWidget {
         color: Colors.black,
         textColor: Colors.white,
         child: Text('INGRESAR'),
-        onPressed: () {},
+        onPressed: signIn,
       ),
     );
   }
@@ -32,25 +63,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 48),
-          decoration: BoxDecoration(color: Color.fromRGBO(255, 251, 193, 1.0)),
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/logo.png',
-                width: 96,
-                height: 96,
-              ),
-              createLabel(41, 'USUARIO'),
-              createFormField('usuario@ejemplo.com', false),
-              createLabel(15, 'CONTRASEÑA'),
-              createFormField('Contraseña', true),
-              createLoginButton(),
-            ],
-          ))),
+      body: Form(
+        key: _formKey,
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 48),
+              decoration:
+                  BoxDecoration(color: Color.fromRGBO(255, 251, 193, 1.0)),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/logo.png',
+                    width: 96,
+                    height: 96,
+                  ),
+                  createLabel(41, 'USUARIO'),
+                  createEmailFormField('usuario@ejemplo.com', false),
+                  createLabel(15, 'CONTRASEÑA'),
+                  createPasswordFormField('Contraseña', true),
+                  createLoginButton(),
+                ],
+              )))),
     );
+  }
+
+  Future<void> signIn() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      try {
+        print(_email);
+        FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email.trim(), password: _password.trim())).user;
+        Navigator.push(context, MaterialPageRoute(builder:  (context) => Home()));      
+      } catch (e) {
+        print(e.message);
+      }
+    }
   }
 }
